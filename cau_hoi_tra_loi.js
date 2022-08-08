07279f1d9e6f695a4e902977ab99495dadc00b5f
unknown <you@example.com>
2022-08-08 09:58:09 +0700

    câu hỏi và trả lời

---
+++ cau_hoi_tra_loi.js
@@ -0,0 +1,7 @@
+let cautraloi = prompt('What is the "official" name of JavaScript?');
+ if (cautraloi == 'ECMAScript') {
+     alert('Right!')
+ }
+ else{
+     alert("You don't know? ECMAScript!")
+ };
